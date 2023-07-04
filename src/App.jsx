19a847d8dbdd2b16931fdd29a16d12019f8a6df3
Aleksandr-Pod <stdfire@gmail.com>
2023-07-04 11:30:58 +0300

    Tailwind-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-slate-400">
-      <h1 className="text-txt-main">Vite + React</h1>
-      <div className="p-8">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="bg-slate-400 text-txt-main text-2xl text-center p-20">
+      Hello World
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.jsx"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        "txt-main": "#f28b82"
+        "txt-main": "#fFF000"
       }
     },
   },
