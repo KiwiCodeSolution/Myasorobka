19369029bb01349c8a3bd18d6cac7d3fec63858b
Aleksandr-Pod <stdfire@gmail.com>
2023-07-04 10:32:06 +0300

    TailWind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.jsx"],
+  theme: {
+    extend: {
+      colors: {
+        "txt-main": "#f28b82"
+      }
+    },
+  },
+  plugins: [],
+}
+
