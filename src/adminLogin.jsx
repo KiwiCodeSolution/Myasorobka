0edd4b2ec4da9793ca8394f91d62eec9d17eea70
Aleksandr-Pod <stdfire@gmail.com>
2023-07-10 17:42:33 +0300

    popup if server error + loader

--- src/adminLogin.jsx
+++ src/adminLogin.jsx
@@ -1,5 +1,7 @@
 import { observer } from "mobx-react-lite";
 import auth from "./store/auth";
+import PopupOverlay from "./components/UIKit/PopupOverlay";
+import RingLoader from "react-spinners/RingLoader";
 
 const AdminLogin = observer(() => {
   console.log("isAuth:", auth.isAuth);
@@ -8,13 +10,36 @@ const AdminLogin = observer(() => {
     console.log("handle login");
     auth.loginAction();
   }
+
   return (
     <>
-      <h2 className="text-center p-4">Админ - форма авторизации</h2>
-      <hr />
-      <button className="w-full text-center py-4" type="button" onClick={handleLogin}>Login</button>
-      <button className="w-full text-center" type="button" onClick={(() => auth.toggleIsAuth())}>Submit</button>
-      {auth.isAuth && <h2 className="text-center p-4">Authorized</h2>}
+      {auth.isLoading ?
+        (<div className="flex justify-center items-center">
+          <RingLoader color="red" loading={auth.isloading} size={120} />
+        </div>) :
+        (<>
+          <h2 className="text-center p-4">Админ - форма авторизации</h2>
+          <hr />
+          <button
+            className="w-full text-center py-4"
+            type="button"
+            onClick={handleLogin}
+          >
+            Login with server (localhost)
+          </button>
+          <button
+            className="w-full text-center"
+            type="button"
+            onClick={(() => auth.toggleIsAuth())}
+          >
+            enter wothout server
+          </button>
+          {auth.error && <PopupOverlay closeByClickOnOverlay closeByPressEsc onClose={() => auth.clearError()}>
+            <h2 className="p-20 bg-bg-orange text-2xl rounded-full">{auth.error}</h2>
+          </PopupOverlay>
+          }
+        </>)
+      }
     </>
   )
 });
--- src/store/auth.js
+++ src/store/auth.js
@@ -1,36 +1,46 @@
-import { makeAutoObservable, runInAction } from "mobx"
+import { makeAutoObservable, runInAction } from "mobx";
+import { makePersistable } from "mobx-persist-store";
 import login from "../API/login";
 
 class Auth {
   isAuth = false;
   token = "";
   isLoading = false;
-  message = "";
+  error = "";
 
   constructor() {
-    makeAutoObservable(this)
+    makeAutoObservable(this);
+    makePersistable(this, {name: "auth", properties: ["isAuth", "token", "error"], storage: window.localStorage})
   }
 
   toggleIsAuth = () => {
     this.isAuth = !this.isAuth
   }
+  clearError = () => {
+    this.error = ""
+  }
   loginAction = async () => {
-    this.isLoading = true;
-    const { message, token } = await login();
-    runInAction(() => {
+    try {
+      this.isLoading = true;
+      const { message, token } = await login();
+      runInAction(() => {
+        this.isLoading = false;
+
+        if (token) {
+          this.token = token;
+          this.isAuth = true;
+          this.error = "";
+
+        } else {
+          this.token = "";
+          this.error = message;
+        }
+      })
+    }
+    catch (error) {
+      this.error = error.message;
       this.isLoading = false;
-
-      if (token) {
-        this.token = token;
-        this.isAuth = true;
-        this.message = "";
-
-      } else {
-        this.token = "";
-        this.message = message;
-      }
-    })
-    
+    }
   }
   
 }
