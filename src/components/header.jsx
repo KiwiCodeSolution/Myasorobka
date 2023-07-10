2679e3df67fc1f369f22a7fa8f2619dfb690871d
Aleksandr-Pod <stdfire@gmail.com>
2023-07-10 10:52:06 +0300

    soc icons map

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,20 @@
 import PropTypes from "prop-types";
 import ButtonMain from "./UIKit/button";
+import fbIcon from "../icons/facebook.svg";
+import tgIcon from "../icons/telegram.svg";
+import instaIcon from "../icons/instagram.svg";
 
-const NAVELEMENTS = [
+const NAV_ELEMENTS = [
   { name: "Про нас", linkId: "about" },
   { name: "Магазин", linkId: "shop" },
   { name: "Контакти", linkId: "contacts" },
-  { name: "Оптовим покупцям", linkId: "wholesale" },
+  // { name: "Оптовим покупцям", linkId: "wholesale" },
 ];
+const SOC_ELEMENTS = [
+  { key: "fb", imgSrc: fbIcon, alt: "facebook icon" },
+  { key: "tg", imgSrc: tgIcon, alt: "telegram icon" },
+  { key: "inst", imgSrc: instaIcon, alt: "instagram icon" }
+]
 
 const Header = ({ toggleModal }) => {
   return (
@@ -16,10 +24,10 @@ const Header = ({ toggleModal }) => {
         <div className="flex py-5 items-center">
           <nav className="mr-11">
             <ul className="h-20 flex items-center gap-x-11 ">
-              {NAVELEMENTS.map((el) => (
+              {NAV_ELEMENTS.map((el) => (
                 <li
                   key={el.name}
-                  className="cursor-pointer text-base hover:text-txt-main-yellow focus:text-txt-main-yellow"
+                  className="cursor-pointer text-xl hover:text-txt-main-yellow focus:text-txt-main-yellow"
                 >
                   {el.name}
                 </li>
@@ -31,10 +39,17 @@ const Header = ({ toggleModal }) => {
           </ButtonMain>
         </div>
 
-        <ul className="absolute bottom-[85px] left-[10%] flex flex-col gap-y-5">
-          <li className="rounded-full hover:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)] focus:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)]">
+        <ul className="absolute bottom-[85px] left-[10%] flex flex-col gap-y-6">
+          {SOC_ELEMENTS.map(({ key, imgSrc, alt }) => {
+            <li key={key} className="rounded-full hover:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)] focus:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)]">
+              <a href="" className="">
+                <img src={imgSrc} alt={alt} />
+              </a>
+            </li>
+          })}
+          {/* <li className="rounded-full hover:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)] focus:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)]">
             <a href="" className="">
-              <img src="../../src/icons/facebook.svg" alt="facebook icon" />
+              <img src={fbIcon} alt="facebook icon" />
             </a>
           </li>
           <li className="rounded-full hover:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)] focus:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)]">
@@ -46,7 +61,7 @@ const Header = ({ toggleModal }) => {
             <a href="">
               <img src="../../src/icons/instagram.svg" alt="instagram icon" />
             </a>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
