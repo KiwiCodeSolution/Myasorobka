5f3d64b6bf8f55fdda18f73e344b92453bca3004
Aleksandr-Pod <stdfire@gmail.com>
2023-07-10 11:47:22 +0300

    propTypes in socialBlock icons

--- src/components/socBlock.jsx
+++ src/components/socBlock.jsx
@@ -1,5 +1,5 @@
 import * as icons from "../icons/iconComponent";
-// import { ReactPropTypes } from "react";
+import PropTypes from "prop-types";
 
 const SOC_ELEMENTS = [
   { key: "fb", Icon: icons.Fb },
@@ -7,13 +7,18 @@ const SOC_ELEMENTS = [
   { key: "inst", Icon: icons.Insta }
 ]
 
-// eslint-disable-next-line react/prop-types
-export const SocialLinksBlock = ({ direction }) => (
-  <ul className={`absolute bottom-[85px] left-[10%] flex ${direction === "col" ? "flex-col" : "flex-raw"} gap-y-6`}>
-  {SOC_ELEMENTS.map(({ key, Icon }) => (
-    <li key={key} className="rounded-full hover:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)] focus:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)]">
-      <a href="" className=""><Icon /></a>
-    </li>
-  ))}
-</ul>
-) 
+export const SocialLinksBlock = ({ flexDirection, left = "10%", bottom = "85px" }) => (
+  <ul className={`absolute bottom-[${bottom}] left-${left} flex ${flexDirection === "col" ? "flex-col" : "flex-row"} gap-y-6`}>
+    {SOC_ELEMENTS.map(({ key, Icon }) => (
+      <li key={key} className="rounded-full hover:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)] focus:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)]">
+        <a href="" className=""><Icon /></a>
+      </li>
+    ))}
+  </ul>
+);
+
+SocialLinksBlock.propTypes = {
+  flexDirection: PropTypes.oneOf(["col", "row"]),
+  left: PropTypes.string,
+  bottom: PropTypes.string,
+}
