2c89a23d48d87445d831feaa3a3a67182f193db9
Aleksandr-Pod <stdfire@gmail.com>
2023-07-10 11:36:02 +0300

    soc icons block

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,53 +1,33 @@
 import PropTypes from "prop-types";
 import ButtonMain from "./UIKit/button";
-import fbIcon from "../icons/facebook.svg";
-import tgIcon from "../icons/telegram.svg";
-import instaIcon from "../icons/instagram.svg";
 
 const NAV_ELEMENTS = [
   { name: "Про нас", linkId: "about" },
   { name: "Магазин", linkId: "shop" },
   { name: "Контакти", linkId: "contacts" },
   // { name: "Оптовим покупцям", linkId: "wholesale" },
-];
-const SOC_ELEMENTS = [
-  { key: "fb", imgSrc: fbIcon, alt: "facebook icon" },
-  { key: "tg", imgSrc: tgIcon, alt: "telegram icon" },
-  { key: "inst", imgSrc: instaIcon, alt: "instagram icon" }
 ]
 
 const Header = ({ toggleModal }) => {
   return (
-    <div>
-      <div className="max-w-[1280px] h-20  mx-auto flex justify-between text-txt-main-white ">
-        <img src="../../src/images/logo.png" alt="Logo" className="w-[76px] h-[76px] ml-20 mt-4" />
-        <div className="flex py-5 items-center">
-          <nav className="mr-11">
-            <ul className="h-20 flex items-center gap-x-11 ">
-              {NAV_ELEMENTS.map((el) => (
-                <li
-                  key={el.name}
-                  className="cursor-pointer text-xl hover:text-txt-main-yellow focus:text-txt-main-yellow"
-                >
-                  {el.name}
-                </li>
-              ))}
-            </ul>
-          </nav>
-          <ButtonMain style="redCustom" clickFn={toggleModal}>
-            Замовити смаколики
-          </ButtonMain>
-        </div>
-
-        <ul className="absolute bottom-[85px] left-[10%] flex flex-col gap-y-6">
-          {SOC_ELEMENTS.map(({ key, imgSrc, alt }) => (
-            <li key={key} className="rounded-full hover:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)] focus:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)]">
-              <a href="" className="">
-                <img src={imgSrc} alt={alt} />
-              </a>
-            </li>
-          ))}
-        </ul>
+    <div className="max-w-[1280px] h-20  mx-auto flex justify-between text-txt-main-white ">
+      <img src="../../src/images/logo.png" alt="Logo" className="w-[76px] h-[76px] ml-20 mt-4" />
+      <div className="flex py-5 items-center">
+        <nav className="mr-11">
+          <ul className="h-20 flex items-center gap-x-11 ">
+            {NAV_ELEMENTS.map((el) => (
+              <li
+                key={el.name}
+                className="cursor-pointer text-xl hover:text-txt-main-yellow focus:text-txt-main-yellow"
+              >
+                {el.name}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <ButtonMain style="redCustom" clickFn={toggleModal}>
+          Замовити смаколики
+        </ButtonMain>
       </div>
     </div>
   );
