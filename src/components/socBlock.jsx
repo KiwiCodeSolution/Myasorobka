b1fbd5dfcaca4144569a4190b18ee829965bb89b
Aleksandr-Pod <stdfire@gmail.com>
2023-07-10 12:19:03 +0300

    fixes

--- src/components/socBlock.jsx
+++ src/components/socBlock.jsx
@@ -7,15 +7,18 @@ const SOC_ELEMENTS = [
   { key: "inst", Icon: icons.Insta }
 ]
 
-export const SocialLinksBlock = ({ flexDirection = "col", left = "10%", bottom = "85px" }) => (
-  <ul className={`absolute bottom-[${bottom}] left-[${left}] flex ${flexDirection === "col" ? "flex-col" : "flex-row"} gap-y-6`}>
-    {SOC_ELEMENTS.map(({ key, Icon }) => (
-      <li key={key} className="rounded-full hover:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)] focus:shadow-[0_5px_20px_-5px_rgba(251,221,61,1)]">
-        <a href="" className=""><Icon /></a>
-      </li>
-    ))}
-  </ul>
-);
+export const SocialLinksBlock = ({ flexDirection = "col" }) => {
+  const shadow = "shadow-[0_5px_20px_-5px_rgba(251,221,61,1)]";
+  return (
+    <ul className={`absolute flex ${flexDirection === "col" ? "flex-col gap-y-6 bottom-[85px] left-[70px]" : "flex-row gap-x-5"} `}>
+      {SOC_ELEMENTS.map(({ key, Icon }) => (
+        <li key={key} className={`rounded-full hover:${shadow} focus:${shadow}`}>
+          <a href="" className=""><Icon /></a>
+        </li>
+      ))}
+    </ul>
+  )
+};
 
 SocialLinksBlock.propTypes = {
   flexDirection: PropTypes.oneOf(["col", "row"]),
