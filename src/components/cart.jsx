64533ab8464634992714928ee92b2bcd16c33214
Aleksandr Podmazko <stdfire@gmail.com>
2023-07-12 14:17:50 +0300

    orders on cart

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,11 +1,16 @@
 import * as icons from "../icons/iconComponent";
+import { observer } from "mobx-react-lite";
+import ordersStore from "../store/orders";
 
-const Cart = () => {
+const Cart = observer(() => {
+  // console.log(orders.order.products.length)
+
   return (
     <div className="absolute bottom-[60px] right-[60px]">
       <icons.Cart />
-      <p className="absolute bottom-4 right-4 bg-bg-red w-8 h-8 rounded-full text-xl text-center">6</p>
+      <p className="absolute bottom-4 right-4 bg-bg-red w-8 h-8 rounded-full text-xl text-center">{ordersStore.order.products.length}</p>
     </div>
   )
-}
+})
+
 export default Cart;
