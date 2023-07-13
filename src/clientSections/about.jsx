b9595885efd0c336a32ab2d43730e7efa2dcb0ec
Aleksandr-Pod <stdfire@gmail.com>
2023-07-14 00:49:34 +0300

    about section

--- src/clientSections/about.jsx
+++ src/clientSections/about.jsx
@@ -1,6 +1,22 @@
 const About = () => {
   return (
-    <h2>About section</h2>
+    <div className="bg-about bg-no-repeat bg-cover h-[920px] relative">
+      <h2 className="pl-[120px] pt-[60px] text-txt-main-white text-3xl">
+        Трохи про М’ясорОбку
+      </h2>
+      <div className="bg-bg-orange w-[420px] rounded-3xl absolute top-[176px] left-[120px] py-3 px-5">
+        <p className="text-center text-xl font-medium pb-2">Початок</p>
+        <p className="font-medium">Наша історія починається з мрії Джеймса Робертсона - мрії про власний бізнес, що принесе користь нашому рідному селу. Вже змалечку він відчував пристрасть до м&apos;яса і проводив години на кухні, творячи неперевершені страви для своїх родичів і друзів. З кожним новим днем, його любов до готування м&apos;яса зростала, і Джеймс почав думати про способи поділитися своїми кулінарними талантами з більшим колом людей.</p>
+      </div>
+      <div className="bg-bg-light-orange w-[468px] rounded-3xl absolute top-[586px] left-[300px] py-3 px-6">
+        <p className="text-center text-xl font-medium pb-2">Ідея</p>
+        <p className="font-medium">Так виникла ідея заснувати мясний онлайн магазин - місце, де всі зможуть насолоджуватися свіжим, якісним м&apos;ясом прямо у своїх домівках. Разом із своєю сім&apos;єю, Джеймс зробив перший крок до реалізації своєї мрії. Він об&apos;єднався з місцевими фермерами, які забезпечували найкращу якість м&apos;яса.</p>
+      </div>
+      <div className="bg-bg-brown w-[428px] rounded-3xl absolute top-[385px] right-[143px] py-3 px-6 text-txt-main-white">
+        <p className="text-center text-xl font-medium pb-2">Правила</p>
+        <p className="font-medium">Ретельний відбір продукції та дотримання найвищих стандартів якості стали основними принципами нашого бізнесу. Сьогодні наш онлайн магазин м&apos;ясних виробів пропонує широкий вибір свіжого м&apos;яса та дельікатесів, які ми з гордістю доставляємо прямо до ваших дверей.</p>
+      </div>
+    </div>
   )
 }
 export default About;
--- src/clientSections/favourite.jsx
+++ src/clientSections/favourite.jsx
@@ -5,7 +5,9 @@ import rosemary from "../images/rosemary.png";
 const Favourite = () => {
   return (
     <div className="bg-bg-main relative pb-[134px] h-[600px]">
-      <h2 className="pl-[120px] text-3xl text-txt-main-white pt-12 pb-4">Найулюбленіші смаколики покупців</h2>
+      <h2 className="pl-[120px] text-3xl text-txt-main-white pt-12 pb-4">
+        Найулюбленіші смаколики покупців
+      </h2>
       <img src={hotPepper} alt="hot pepper" className="absolute top-[-64px] z-0" />
       <img src={rosemary} alt="rosemary" className="absolute right-4 bottom-0"/>
       <div className="absolute">
