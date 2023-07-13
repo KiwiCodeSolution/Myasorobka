4f34039aa3f2c9c7acd9a5ebd96e6af941b58e2e
Aleksandr-Pod <stdfire@gmail.com>
2023-07-14 00:03:18 +0300

    favourite section

--- src/components/roundNumbers.jsx
+++ src/components/roundNumbers.jsx
@@ -1,6 +1,6 @@
 const roundNumbers = ({ activeBtn, setQtty }) => {
   const buttons = [1, 2, 5];
-  const genStyles = "w-8 h-8 border border-black rounded-full text-center py-1"
+  const genStyles = "w-8 h-8 border border-black rounded-full text-center py-1 cursor-pointer"
   const activeStyle = "bg-bg-light-orange"
   return (
     <div className="flex gap-x-8 justify-center my-2">
