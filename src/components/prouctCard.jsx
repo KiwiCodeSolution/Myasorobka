0b139e850c3d532acf0c4ced0d75bf36138b3353
Aleksandr-Pod <stdfire@gmail.com>
2023-07-21 14:00:30 +0300

    admin Product cards

--- src/components/prouctCard.jsx
+++ src/components/prouctCard.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import testPicture from "../tempPicture/unsplash_6JQMjhqpVhE.jpg";
-import * as icons from "../icons/iconComponent";
+import {Line, FavouriteIcon, TrashIcon} from "../icons/iconComponent";
 import RoundNumbers from "./roundNumbers";
 import ButtonMain from "./UIKit/button";
 import ordersStore from "../store/orders";
@@ -16,29 +16,55 @@ const ProductCard = ({ product }) => {
   const addToCart = () => {
     ordersStore.addToCart(product, qttyBtn);
   };
+  const editProduct = () => {
+    console.log("edit product");
+  };
+  const deleteProduct = () => {
+    console.log("delete product");
+  };
+  const addToFavourite = () => {
+    console.log("addToFavourite");
+  }
 
   return (
-    <div className="h-[356px] w-[216px] rounded-3xl bg-bg-white mx-auto">
+    <div className={`${admin ? "h-[316px]" : "h-[356px]"} w-[216px] rounded-3xl bg-bg-white mx-auto`}>
       <img src={product.img || testPicture} alt={"product image"} className={"h-[168px] rounded-t-3xl"} />
       <p className="px-2 py-1 text-center font-bold">{product.name}</p>
-      <icons.Line active />
+      <Line active />
       <p className="px-2 py-1 text-center font-basic">
         {product.price} грн / {product.unit}
       </p>
-      {product.discount_price ? (
-        <p className="px-2 py-1 text-center text-txt-grey font-basic">
+      {product.discount_price ?
+        (<p className="px-2 py-1 text-center text-txt-grey font-basic">
           {product.discount_price} грн / {product.unit}
-        </p>
+        </p>)
+        :
+        (<p className="px-2 py-1 text-center text-txt-grey font-basic">_________________</p>)
+      }
+      
+      {admin ? (
+        <>
+          <div className="flex justify-center">
+            <ButtonMain style={"whiteSmall"} clickFn={editProduct}>
+              Редагувати
+            </ButtonMain>
+          </div>
+          <button className="absolute top-[132px] right-6 w-6 h-6 bg-bg-black rounded" onClick={addToFavourite}><FavouriteIcon /></button>
+          <button className="absolute top-3 right-6 w-6 h-6 bg-bg-black rounded" onClick={deleteProduct}><TrashIcon/></button>
+        </>
       ) : (
-        <p className="px-2 py-1 text-center text-txt-grey font-basic">_________________</p>
+        <>
+          <RoundNumbers activeBtn={qttyBtn} setQtty={setQttyBtn} />
+          <div className="flex justify-center">
+            <ButtonMain style={"addToCart"} clickFn={addToCart}>
+              Додати у кошик
+            </ButtonMain>
+          </div>
+        </>
       )}
-      <RoundNumbers activeBtn={qttyBtn} setQtty={setQttyBtn} />
-      <div className="flex justify-center">
-        <ButtonMain style={"addToCart"} clickFn={addToCart}>
-          Додати у кошик
-        </ButtonMain>
-      </div>
+        
     </div>
+    
   );
 };
 
