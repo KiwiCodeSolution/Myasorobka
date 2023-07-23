6b3a93c6c658807aaa7ebaf3c613417486501c4b
Aleksandr Podmazko <stdfire@gmail.com>
2023-07-23 15:00:09 +0300

    addProduct close on success

--- src/store/products.js
+++ src/store/products.js
@@ -43,7 +43,7 @@ class Products {
     })
     this.getProductsAction();
     adminState.setMessage("Продукт додан успішно")
-    return;
+    return true;
   }
 
   updateProductAction = async (product) => {
