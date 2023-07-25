6821f34b9ea3901932745af79d25ac98eb68f23f
Aleksandr-Pod <stdfire@gmail.com>
2023-07-25 17:14:14 +0300

    remove some console.log

--- src/components/PreviewImgField.jsx
+++ src/components/PreviewImgField.jsx
@@ -5,8 +5,8 @@ import { baseServerURL } from "../config/url";
 const PreviewImgField = observer(() => {
   const { uploadedImages: imgUrl } = productStore;
   // console.log("url request:",`${baseServerURL}${imgUrl[0]}`)
-  console.log("uploaded images:", imgUrl)
-  return imgUrl && 
+  // console.log("uploaded images:", imgUrl)
+  return imgUrl.length &&
     <img src={`${baseServerURL}${imgUrl.at(-1)}`} alt="preview image" className={"border border-txt-main-white text-txt-main-white text-center w-[162px] h-[192px] object-contain rounded-3xl"}/>
   
 })
