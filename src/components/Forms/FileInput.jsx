2fae88455f2e216eb0780f29f2f74c890a198839
Oleksandr Khardin <khporter1984@gmail.com>
2023-08-06 12:50:06 +0200

    stylize drop zone

--- src/components/Forms/FileInput.jsx
+++ src/components/Forms/FileInput.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
 import PropTypes from "prop-types";
+import { useController } from "react-hook-form";
 import { useDropzone } from "react-dropzone";
 
-import { useController } from "react-hook-form";
+import { PlusInCircle, DnD } from "../../icons/iconComponent";
 
 const FileInput = ({ name, control, onInputChange }) => {
   const {
@@ -24,9 +25,20 @@ const FileInput = ({ name, control, onInputChange }) => {
       <div className="relative w-full flex justify-center">
         <div
           {...getRootProps()}
-          className="flex justify-center items-center w-[162px] h-[192px] rounded-md cursor-pointer text-[black] font-bold bg-[pink]"
+          className="group flex justify-center items-center w-[162px] h-[192px] rounded-3xl cursor-pointer 
+          text-txt-main-black font-bold bg-bg-white outline-none "
         >
-          {isDragActive ? "drag" : "click"}
+          {isDragActive ? (
+            <DnD className="w-2/3 h-2/3 text-bg-orange border-[3px]border-bg-orange" />
+          ) : (
+            <div
+              className="flex flex-col gap-4 items-center group-hover:text-bg-orange
+            group-focus:text-bg-orange transition-all duration-250"
+            >
+              <PlusInCircle className="fill-[tomato]" />
+              <p className="text-xl text-center">Додати зображення</p>
+            </div>
+          )}
         </div>
         <input
           type="file"
@@ -35,9 +47,13 @@ const FileInput = ({ name, control, onInputChange }) => {
           {...getInputProps()}
           className="sr-only"
         />
-        {fieldState.error && (
+        {fieldState.error ? (
           <p className="absolute bottom-0 translate-y-full text-sm font-bold text-[red]">
             {fieldState.error.message}
+          </p>
+        ) : (
+          <p className="absolute bottom-0 translate-y-full text-sm text-txt-main-white ">
+            клацніть по кнопці або перетягніть файл
           </p>
         )}
       </div>
