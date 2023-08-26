3f61733951c403cdb866a7d94586a19cb92a9939
Evgenija-P <e.a.poduzova@gmail.com>
2023-08-26 21:24:14 +0300

    add swiper in product card

---
+++ src/components/prouctCardImages.jsx
@@ -0,0 +1,77 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/rules-of-hooks */
+import PropTypes from "prop-types";
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+import { FreeMode, Navigation, Thumbs, Controller } from "swiper/modules";
+
+const ProuctCardImages = ({ data }) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState();
+  const [firstSwiper, setFirstSwiper] = useState();
+  const [secondSwiper, setSecondSwiper] = useState();
+  const swiper1Ref = useRef(null);
+  const swiper2Ref = useRef();
+
+  useLayoutEffect(() => {
+    if (swiper1Ref.current !== null) {
+      swiper1Ref.current.controller.control = swiper2Ref.current;
+    }
+  }, []);
+
+  return (
+    <div className="w-[400px] h-[536px] flex flex-col justify-between gap-y-3 p-2 relative images-swiper">
+      <Swiper
+        onSwiper={(swiper) => {
+          if (swiper1Ref.current !== null) {
+            swiper1Ref.current = swiper;
+          }
+        }}
+        controller={{ control: secondSwiper }}
+        spaceBetween={50}
+        slidesPerView={1}
+        grabCursor={true}
+        navigation={true}
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+        }}
+        modules={[FreeMode, Navigation, Thumbs, Controller]}
+        className="w-full h-[70%] mySwiperOne rounded-lg"
+      >
+        {data.map((el) => (
+          <SwiperSlide key={el.id}>
+            <img src={el.url} alt="swiper gallary" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Swiper
+        controller={{ control: firstSwiper }}
+        loop={false}
+        spaceBetween={10}
+        slidesPerView={3}
+        watchSlidesProgress
+        touchRatio={0.2}
+        slideToClickedSlide={true}
+        onSwiper={setThumbsSwiper}
+        modules={[Navigation, Thumbs, Controller]}
+        className="w-full min-h-fit mySwiper"
+      >
+        {data.map((el) => (
+          <SwiperSlide key={el.id} className="opacity-70 ">
+            <img src={el.url} alt="swiper gallary" className="rounded-lg" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+ProuctCardImages.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+export default ProuctCardImages;
