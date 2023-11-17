2d02ed056fd61f34f471c31f7aac1a8c4701cf8c
AlexeyTolmachov <taar12sh@gmail.com>
2023-11-17 11:12:57 +0200

    changed schema

--- src/validationSchemas/addProductSchema.js
+++ src/validationSchemas/addProductSchema.js
@@ -3,9 +3,9 @@ import * as Yup from "yup";
 import constants from "../helpers/constants";
 
 const addProductSchema = Yup.object({
-  name: Yup.string().max(20, "Максимум 20 символів").required(constants.REQUIRED_FIELD),
+  name: Yup.string().max(200, "Максимум 200 символів").required(constants.REQUIRED_FIELD),
 
-  category: Yup.string().max(20, "Максимум 20 символів").required(constants.REQUIRED_FIELD),
+  category: Yup.string().max(50, "Максимум 50 символів").required(constants.REQUIRED_FIELD),
 
   price: Yup.string().required(constants.REQUIRED_FIELD),
 
@@ -13,7 +13,7 @@ const addProductSchema = Yup.object({
 
   info: Yup.string().max(50, "Максимум 50 символів").required(constants.REQUIRED_FIELD),
 
-  description: Yup.string().max(100, "Максимум 100 символів"),
+  description: Yup.string().max(200, "Максимум 200 символів"),
 });
 
 export default addProductSchema;
