7b01144bf23da891091066bc6c13c993da984e15
AlexeyTolmachov <taar12sh@gmail.com>
2023-11-17 11:14:30 +0200

    changed the url

--- src/config/url.js
+++ src/config/url.js
@@ -1,2 +1,3 @@
-export const baseServerURL = "http://localhost:5000/";
+// export const baseServerURL = "http://localhost:5000/";
 // export const baseServerURL = "https://kiwicode.tech/myasorobka/api/";
+export const baseServerURL = "https://myasorobka.com/api/";
